b76476c6cb508cc8b375987565bca333476a7c8b
devripon <developershahadathussainripon@gmail.com>
2023-03-18 23:13:35 +0600

    Javascript Starter Tamplate env setup with es6 Webpack and Babel

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "boxicons/css/boxicons.min.css";
+import "./index.css";
+import "./main.scss"
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const HtmlWebpack = require("html-webpack-plugin");
+/**
+ * [exports description]
+ * webpack configuration
+ * make ready for production with webpack
+ */
+module.exports = {
+	mode: "development",
+	entry: path.resolve(__dirname, "src/index.js"),
+	output: {
+		path: path.resolve(__dirname, "bundle"),
+		filename: "bundle.js",
+	},
+	/**
+	 * @var		mixed	modul
+	 * define rules for modules
+	 */
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+			{
+				test: /\.(css|scss)$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+		],
+	},
+	/**
+	 * Devserver configuration for project running in development
+	 */
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, "bundle"),
+		},
+		port: 3000,
+		open: true,
+		hot: true,
+		compress: true,
+		historyApiFallback: true,
+	},
+	/**
+	 * Give Plugin Suppoort
+	 */
+	plugins: [
+		/**
+		 * [HtmlWebpack description]
+		 * Html Webpack Plugin configuration
+		 */
+		new HtmlWebpack({
+			title: "Es6 environment setup with Babel and Webpack",
+			filename: "index.html",
+			template: "public/index.html",
+		}),
+	],
+};
